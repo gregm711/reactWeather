b29e97709d85f496ecd49f8bea4b7972cddd959f
gregm711 <miller.7285osu@gmail.com>
2016-12-01 01:37:45 -0500

    added in gitignore

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+
+var objOne = {
+	name: 'Greg',
+	location: 'cincy'
+};
+var objTwo = {
+	age:'22',
+	...objOne
+};
+
+
 ReactDOM.render(
 	<h1> Boilerplate app</h1>,
     document.getElementById('app')
