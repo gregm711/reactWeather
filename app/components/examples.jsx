0f47ccfb3b4a9173ba1bd9fa2c730c260e571edd
gregm711 <miller.7285osu@gmail.com>
2016-12-01 14:13:57 -0500

    added in custom styling

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -5,8 +5,8 @@ var {Link} = require('react-router');
 var Examples = (props) =>  {
 		return (
 		<div>
-			<h1 className="text-center"> Examples</h1>
-			<p> here are a few examples</p>
+			<h1 className="text-center page-title"> Examples</h1>
+			<p className="text-center"> here are a few examples</p>
 			<ol>
 				<li>
 					<Link to="/?location=london">London, UK</Link>
